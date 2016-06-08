9f94e962e0699236ad98082eff4ce7332e456dea
gabzaaf <zafrani.prog@gmail.com>
2016-06-08 22:59:59 +0200

    I have fix the merge

--- app/src/main/java/zaafranigabriel/recipy/DisplayMap.java
+++ app/src/main/java/zaafranigabriel/recipy/DisplayMap.java
@@ -12,19 +12,13 @@ import org.json.JSONObject;
 
 import zaafranigabriel.recipy.WebService.ApiLocation;
 
-=======
-import android.view.Menu;
-import android.view.MenuItem;
 
->>>>>>> 14969a384279795f32002dad1598bcd3f123f060
 public class DisplayMap extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_map);
-<<<<<<< HEAD
-        String val2 = null;
         try{
             String val = new ApiLocation().execute("8 rue du vieil armand 95200 sarcelles").get();
             JSONObject jsonObject = new JSONObject(val);
@@ -34,10 +28,7 @@ public class DisplayMap extends Activity {
             JSONObject location = geometry.getJSONObject("location");
             double lat = location.getDouble("lat");
             double lng = location.getDouble("lng");
-            //JSONObject listElement  = listeResult.getJSONObject("location");
-            //val2 = listElement.toString();
 
-            String toto = "2";
         }catch(Exception e){
             Log.d("ElementID",val2);
         }
@@ -47,29 +38,3 @@ public class DisplayMap extends Activity {
 
 
 
-=======
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_display_map, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
->>>>>>> 14969a384279795f32002dad1598bcd3f123f060
